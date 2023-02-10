981d8cba6676d39fae5c01969097167bb5a67f16
Martin Ivanov <xeartybg@gmail.com>
2023-02-10 11:50:24 +0200

    make a win32 platform file

--- xjit/src/xjit/jit_internal.cpp
+++ xjit/src/xjit/jit_internal.cpp
@@ -1,26 +1,6 @@
 #include "jit.h"
 #include "jit_helpers.h"
 
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-
-const Byte* X64Generator::GetExecutableMemory() const
-{
-    SYSTEM_INFO system_info;
-    GetSystemInfo(&system_info);
-    const auto page_size = system_info.dwPageSize;
-
-    Byte* buffer = (Byte*)VirtualAlloc(NULL, page_size, MEM_COMMIT, PAGE_READWRITE);
-    if (buffer)
-    {
-        memcpy(buffer, executable_memory.data(), executable_memory.size());
-        DWORD old_protection;
-        VirtualProtect(buffer, page_size, PAGE_EXECUTE_READ, &old_protection);
-    }
-
-    return buffer;
-}
-
 void X64Generator::SetIdentifierRegister(const IPLString& name, int reg)
 {
     identifier_to_register[name] = reg;
