a17e68b084e60b098a9cfcf8770d65a1a0452a46
Martin Ivanov <xeartybg@gmail.com>
2023-02-11 12:22:52 +0200

    fix test

--- JSImpl/test/JitTests.cpp
+++ JSImpl/test/JitTests.cpp
@@ -508,7 +508,7 @@ TEST_F(JitTest, WhileContinueOnNumber)
 	const auto* executable_code = Compile(
 		" function func() {                        "
 		"     var i = -1;                          "
-		"     while (i < 10) {                     "
+		"     while (i < 10 - 1) {                 "
 		"         i = i + 1;                       "
 		"         print | i;                       "
 		"         if (i == 5) {                    "
